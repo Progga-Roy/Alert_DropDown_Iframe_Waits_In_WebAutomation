6e1bc27d90f2ac8a5f30daf4233a6fa9752210db
Progga-Roy <proggaroy2000@gmail.com>
2025-06-03 17:31:02 +0600

    Simple Alert Locate

--- src/test/java/javas/example/Alerts.java
+++ src/test/java/javas/example/Alerts.java
@@ -1,21 +1,40 @@
 package javas.example;
 
 
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 import java.time.Duration;
 
 
 public class Alerts extends DriverSetUp{
+
     @Test
     public void webPageLoad(){
+        // Page load and get the title and url
         driver.get("https://demoqa.com/alerts");
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
-        System.out.println(driver.getTitle());
-        System.out.println(driver.getCurrentUrl());
+        System.out.println("Page Title : " + driver.getTitle());
+        System.out.println("Current Url : " + driver.getCurrentUrl());
+
+        //Locate first alert
+        WebElement clickAlert1 =  driver.findElement(By.xpath("//button[@id='alertButton']"));
+        clickAlert1.click();
+        WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(2));
+        wait.until(ExpectedConditions.alertIsPresent());
+        Alert alert = driver.switchTo().alert();
+        System.out.println("Alert text : " + alert.getText());
+        alert.accept();
 
 
 
     }
 
-}
+
+    }
+
+
