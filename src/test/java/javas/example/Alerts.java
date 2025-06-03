200361ec26f4b3142264f0407076675490085060
Progga-Roy <proggaroy2000@gmail.com>
2025-06-03 21:09:00 +0600

    Alert Prompt Box

--- src/test/java/javas/example/Alerts.java
+++ src/test/java/javas/example/Alerts.java
@@ -16,7 +16,7 @@ public class Alerts extends DriverSetUp{
     public void webPageLoad() throws InterruptedException {
         // Page load and get the title and url
         driver.get("https://demoqa.com/alerts");
-//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds());
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
         System.out.println("Page Title : " + driver.getTitle());
         System.out.println("Current Url : " + driver.getCurrentUrl());
 
@@ -27,31 +27,40 @@ public class Alerts extends DriverSetUp{
         wait.until(ExpectedConditions.alertIsPresent());
         Alert alert = driver.switchTo().alert();
 //        Thread.sleep(1000);
-        System.out.println("Alert text : " + alert.getText());
+        System.out.println("Alert text_1 : " + alert.getText());
 //        Thread.sleep(1000);
         alert.accept();
-       //Locate second alert
+
+        //Locate second alert
         driver.findElement(By.cssSelector("#timerAlertButton")).click();
         WebDriverWait wait1 = new WebDriverWait(driver,Duration.ofSeconds(5));
         wait1.until(ExpectedConditions.alertIsPresent());
         Alert alert2 = driver.switchTo().alert();
-        System.out.println("Text : " + alert2.getText());
+        System.out.println("Alert text_2: " + alert2.getText());
         alert2.accept();
 
-
-  driver.findElement(By.cssSelector("#confirmButton")).click();
+// Confirm Box
+        driver.findElement(By.cssSelector("#confirmButton")).click();
 ////  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
 //        WebDriverWait waitToClick = new WebDriverWait(driver,Duration.ofSeconds(6));
 //        waitToClick.until(ExpectedConditions.elementToBeClickable(By.cssSelector("#confirmButton")));
-  Alert confirmAlert = driver.switchTo().alert();
-        Thread.sleep(1000);
-        System.out.println("Text : " + confirmAlert.getText());
-//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
-        Thread.sleep(1000);
+        Alert confirmAlert = driver.switchTo().alert();
+//        Thread.sleep(1000);
+        System.out.println("Alert text_3" + confirmAlert.getText());
+//        Thread.sleep(1000);
         confirmAlert.dismiss();
-    }
+
+        // Prompt Box
+        driver.findElement(By.xpath("//button[@id='promtButton']")).click();
+        Alert promptBoxAlert = driver.switchTo().alert();
+        promptBoxAlert.sendKeys("Hello, I am there!");
+        System.out.println("Alert text_4 : " + promptBoxAlert.getText());
+        promptBoxAlert.accept();
 
 
     }
+
+
+}
 
 
